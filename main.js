35b7bba3f8d9309471b211ee29190a1c67ec0f82
Christian Robinson <chrisdrobbo@gmail.com>
2021-10-21 15:34:37 +0100

    uploaded correct files

--- main.js
+++ main.js
@@ -1,107 +1,96 @@
-// Add a new book to myLibrary array for testing purposes
-const bookOne = new Book('Pride and Prejudice', 'Jane Austen', 112, false);
-const bookTwo = new Book('Of Mice and Men', 'John Steinbeck', 107, true);
-let myLibrary = [];
-myLibrary.push(bookOne);
-myLibrary.push(bookTwo);
-let myFormData = [];
+let numberList = [...Array(99999).keys()]
 
+let filteredList = numberList.filter( num => checkNum(num))
 
-// Book constructor for making "Book" objects
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+function checkNum(num) {
+    str = num.toString();
+    return checkEachDigit(str)
+}
 
-    this.info = function() {
-      console.log(`${title} by ${author}, ${pages} pages, ` + (read ? 'finished reading' : 'not read yet'));
+function checkEachDigit(str) {
+    // console.log(str)
+    if (str.includes("0")) {
+        return false;
+    } else if (str.includes("1")) {
+        return false;
+    } else if (str.includes("4")) {
+        return false;
+    } else if (str.includes("6")) {
+        return false;
+    } else if (str.includes("8")) {
+        return false;
+    } else if (str.includes("9")) {
+        return false;
+    } else {
+        return true;
     }
-  };
+}
 
-  // Event listeners
-
-  let formBtn = document.querySelector('#formBtn');
-  if (formBtn) {
-    formBtn.addEventListener('click', (e)=>{
-        formData();
-     });
-  };
- 
-
-  function addBookToLibrary(book) {
-      myLibrary.push(book);
-  };
-
-  function displayBooks() {
-    myLibrary.forEach(book => {
-        let bookArray = []
-        Object.keys(book).forEach(key => {
-            bookArray.push(book[key]);
-        });
-        appendItems(bookArray)
-    });
-  };
-
-  // Fucntion to create a html card which displays book information
-  function appendItems(bookArray) {
-      let BookList = document.getElementById("book-list");
-      let newDiv = document.createElement("div");
-      newDiv.classList.add("book");
-      let bookDetails = document.createElement("div");
-      bookDetails.classList.add("book-details")
-      let centeredDiv = document.createElement("div");
-      centeredDiv.classList.add("centered-div");
-      let image = document.createElement("img");
-      image.src = "https://via.placeholder.com/200x250";
-      image.classList.add("book-img");
-      let title = document.createElement("h3");
-      let author = document.createElement("p");
-      let pages = document.createElement("p");
-      let read = document.createElement('p');
-      // Append to parents
-      BookList.appendChild(newDiv);
-      newDiv.appendChild(bookDetails);
-      bookDetails.appendChild(centeredDiv);
-      newDiv.appendChild(image);
-      centeredDiv.appendChild(title);
-      title.innerText = bookArray[0];
-      centeredDiv.appendChild(author);
-      author.innerText = bookArray[1];
-      centeredDiv.appendChild(pages);
-      pages.innerText = bookArray[2];
-      centeredDiv.appendChild(read);
-      read.innerText = bookArray[3];
-  };
-
-  function addBook() {
-      let title = document.getElementById('title');
-      let author = document.getElementById('author');
-      let pages = document.getElementById('author');
-      let read = document.getElementById('read');
-  };
+// group list of eligible numbers by number of digits
+let twoDigitList = filteredList.filter( num => (num.toString().length > 1 && num.toString().length < 3) )
+let threeDigitList = filteredList.filter( num => (num.toString().length > 2 && num.toString().length < 4) )
+let fourDigitList = filteredList.filter( num => (num.toString().length > 3 && num.toString().length < 5) )
+let fiveDigitList = filteredList.filter( num => (num.toString().length > 4 && num.toString().length < 6) )
 
 
-  function formData() {
-      let title = document.getElementById("title").value;
-      myFormData.push(title);
-      let author = document.getElementById("author").value;
-      myFormData.push(author);
-      let pages = document.getElementById("pages").value;
-      myFormData.push(pages);
-      let read = document.getElementById("read").value;
-      console.log(read)
-      myFormData.push(convertStrToBoolean(read));
-      console.log(myFormData)
-  };
-  
-  function convertStrToBoolean(string) {
-      string === string.toLowerCase;
-      if (string === 'yes') {
-          return true;
-      } else if (string === 'no') {
-          return false;
-      } else {
-          return null;
-      }
-  }
+// Find all valid answers when a three digit number (U) times a 2 digit number (AB)
+let productTwoThree = [];
+
+function twoDigitsTimesThreeDigits(twoDigitArray, threeDigitArray) {
+    array = []
+    twoDigitArray.forEach(num1 => {
+        threeDigitArray.forEach(num2 => {
+           array.push(num1 * num2)
+        })
+    })
+    productTwoThree = array.filter(num => (num.toString().length > 4 && num.toString().length < 6 && checkNum(num)));
+}
+
+// Find all valid answers when a three digit number times single digit (B)
+let productThreeTimesB = [];
+
+function threeDigitsTimesB(twoDigitList, threeDigitList) {
+    array = [];
+    threeDigitList.forEach( num1 => {
+        twoDigitList.forEach( num2 => {
+            array.push(num1 * secondDigit(num2));
+        })
+    })
+    // console.log(array);
+    productThreeTimesB = productThreeTimesB.filter( num => checkNum(num))
+};
+
+function secondDigit(num2) {
+    str = num2.toString();
+    number = parseInt(str[1])
+    return number
+}
+
+// Find all valid answers when a three digit number times single digit (A)
+
+let productThreeTimesA = []
+
+function threeDigitsTimesA(twoDigitList, threeDigitList) {
+    array = [];
+    threeDigitList.forEach( num1 => {
+        twoDigitList.forEach( num2 => {
+            array.push(num1 * firstDigit(num2));
+        })
+    })
+    productThreeTimesB = productThreeTimesB.filter( num => checkNum(num))
+};
+
+
+// function calls
+twoDigitsTimesThreeDigits(twoDigitList, threeDigitList);
+threeDigitsTimesB(twoDigitList, threeDigitList)
+
+console.log(productTwoThree);
+// No values that contain the allowed digits.
+console.log(productThreeTimesA);
+// No values that contain the allowed digits.
+console.log(productThreeTimesB);
+
+
+
+
